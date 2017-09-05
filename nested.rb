c0cdf0eb6e85fd2aab4f69098f426fd1caf54826
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-05 04:38:31 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -123,7 +123,6 @@ def adding_to_dennis
         }
      }
 
-		 languages = programmer_hash[:dennis_ritchie][:languages]
-		 languages[1] = "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
 
 end
